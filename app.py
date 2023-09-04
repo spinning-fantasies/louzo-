703c782e9c2b0ccc5c0b84de8153a987098037e4
Mathieu Mescam <mate@e.email>
2023-09-04 09:18:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 
 # Create a table to store hours
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS hours (
+    CREATE TABLE IF NOT EXISTS intake (
         date TEXT,
         medication TEXT,
         hours FLOAT
@@ -20,7 +20,7 @@ while True:
     hours = float(input("Enter intake time : "))
 
     # Insert the data into the database
-    cursor.execute("INSERT INTO hours VALUES (?, ?, ?)", (date, medication, hours))
+    cursor.execute("INSERT INTO intake VALUES (?, ?, ?)", (date, medication, hours))
     conn.commit()
 
     another_entry = input("Do you want to enter another hour? (yes/no): ")

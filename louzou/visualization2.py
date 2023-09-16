e4959ceac824a6adea5bd3b5b91e83a6bdc344d9
Mathieu Mescam <mate@e.email>
2023-09-16 11:42:06 +0200

    Update visualization2.py

--- louzou/visualization2.py
+++ louzou/visualization2.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sqlite3
 
 # Load data
-df = pd.read_csv('data/intake.csv')
+conn = sqlite3.connect('data/louzou.db')
+data = pd.read_sql_query("SELECT date, hours FROM intake", conn)
+print(data)
 
 # Create a scatter plot
 plt.figure(figsize=(10, 6))
-plt.scatter(df['hours'], df['date'], alpha=0.5)
+plt.scatter(data['hours'], data['date'], alpha=0.5)
 plt.xlabel('hours')
 plt.ylabel('date')
 plt.title('Hour of Taking vs. Date')
@@ -15,4 +18,4 @@ plt.grid(True)
 # Set custom x-axis tick labels for better readability
 plt.xticks(range(24), ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'], rotation=45)
 
-plt.savefig("static/intake2.png")
+plt.savefig("louzou/static/intake2.png")

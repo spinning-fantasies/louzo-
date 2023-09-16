b2eed9fcf44b6cd760fb38b3b847094dafb44e23
Mathieu Mescam <mate@e.email>
2023-09-16 11:56:26 +0200

    Update app.py

--- louzou/app.py
+++ louzou/app.py
@@ -1,13 +1,12 @@
-import pdb
-import sqlite3
-
 from flask import Flask, render_template, url_for
+from db import db_conn
 
 app = Flask(__name__)
 
 @app.route('/')
 def calendar():
-    conn = sqlite3.connect('data/louzou.db')
+    
+    conn = db_conn()
     cursor = conn.cursor()
 
     cursor.execute("SELECT date, medication, hours FROM intake")
@@ -17,6 +16,7 @@ def calendar():
 
     # Generate the URL for 'intake.png' in the 'static' folder
     intake_image_url = url_for('static', filename='intake1.png')
+
     intake_image_url2 = url_for('static', filename='intake2.png')
 
     return render_template('calendar.html', hours_data=hours_data, intake_image_url=intake_image_url, intake_image_url2=intake_image_url2)
